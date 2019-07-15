fcc4d882cb39d2a19dca2c67c3b74c7db8502bd4
Aiman Josefsson <eleton@gmail.com>
2019-07-15 17:37:21 +0200

    Add fields in Choose view

--- src/views/Choose.js
+++ src/views/Choose.js
@@ -1,8 +1,34 @@
 import React, { useState } from 'react';
+import { TextField } from '@material-ui/core/';
 
 const Choose = () => {
+  const [player, setPlayer] = useState("");
+  const [character, setCharacter] = useState("");
+
+  const typePlayerName = e => {
+    const player = e.target.value;
+    setPlayer(player);
+  }
+
+  const typeCharacterName = e => {
+    const character = e.target.value;
+    setCharacter(character);
+  }
+
   return <div>
     <h1>Choose</h1>
+    <TextField
+      id="player"
+      label="Player Name"
+      value={player}
+      onChange={typePlayerName}
+    />
+    <TextField
+      id="character"
+      label="Character Name"
+      value={character}
+      onChange={typeCharacterName}
+    />
   </div>
 }
 
